6ce9930379805b9941b5737f1729d62699c866b1
zae-hyeong <p0138@naver.com>
2024-04-10 23:12:53 +0900

    [programmers] 둘만의 암호, add new solution

--- Programmers/둘만의_암호.js
+++ Programmers/둘만의_암호.js
@@ -22,5 +22,12 @@ function solution(s, skip, index) {
   return answer;
 }
 
+function solution2(s, skip, index) {
+    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", 
+                      "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", 
+                      "u", "v", "w", "x", "y", "z"].filter(c => !skip.includes(c));
+    return s.split("").map(c => alphabet[(alphabet.indexOf(c) + index) % alphabet.length]).join("");
+}
+
 console.log(solution("aukks", "wbqd", 5));
 console.log(solution("zac", "wbqd", 1));
