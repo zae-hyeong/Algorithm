431c46f55eed8ad52bea6274c4ccac8589fe85f1
zae-hyeong <p0138@naver.com>
2024-06-02 21:45:18 +0900

    [improve] 가장 많이 받은 선물
    indexOf를 사용하지 않기 위한 index 객체 생성
    중첩 반복문을 사용하지 않고 선물 지수 계산
    - 선물지수를 총합을 통해 계산하려고 했던 부분에서 비효율 발생

--- Programmers/가장_많이_받은_선물.js
+++ Programmers/가장_많이_받은_선물.js
@@ -1,52 +1,44 @@
 function solution(friends, gifts) {
+
   //받은 선물과 준 선물의 개수, 선물지수를 저장할 2차원 배열(i번째 사람의 선물지수는 table[i][i]에 저장된다)
   const table = Array.from({ length: friends.length }, () =>
     Array.from({ length: friends.length }, () => 0)
   );
 
+  //indexOf를 사용하지 않기 위한 index 객체 생성
+  let index = {};
+  for (let i = 0; i < friends.length; i++) {
+    index[friends[i]] = i;
+  }
+
   let giver;
   let receiver;
   for (let i = 0; i < gifts.length; i++) {
     [giver, receiver] = gifts[i].split(" ");
-    table[friends.indexOf(giver)][friends.indexOf(receiver)] += 1;
-  }
+    table[index[giver]][index[receiver]] += 1;
 
-  //선물지수 구하기
-  let numOfReceivedPresents = 0;
-  let numOfGivedPresents = 0;
-
-  for (let i = 0; i < friends.length; i++) {
-    numOfReceivedPresents = 0;
-    numOfGivedPresents = 0;
-
-    for (let j = 0; j < friends.length; j++) {
-      numOfGivedPresents += table[i][j];
-      numOfReceivedPresents += table[j][i];
-    }
-    table[i][i] = numOfGivedPresents - numOfReceivedPresents;
+    // 선물지수
+    table[index[giver]][index[giver]] += 1;
+    table[index[receiver]][index[receiver]] -= 1;
   }
 
   // 받는 선물 개수 구하기
-  const numOfReceivePresentsArray = Array.from(
-    { length: friends.length },
-    () => 0
-  );
+  const NumOfGiftsToReceive = new Array(friends.length).fill(0);
 
   for (let i = 0; i < friends.length; i++) {
     for (let j = 0; j < friends.length; j++) {
       if (i === j) continue;
 
       if (table[i][j] > table[j][i]) {
-        numOfReceivePresentsArray[i] += 1;
-        continue;
+        NumOfGiftsToReceive[i] += 1;
       }
       if (table[i][j] === table[j][i]) {
-        if (table[i][i] > table[j][j]) numOfReceivePresentsArray[i] += 1;
+        if (table[i][i] > table[j][j]) NumOfGiftsToReceive[i] += 1;
       }
     }
   }
 
-  return Math.max(...numOfReceivePresentsArray);
+  return Math.max(...NumOfGiftsToReceive);
 }
 
 console.log(
