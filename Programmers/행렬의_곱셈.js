6460018ea8c14c84d02d759872577ae3c2347bfe
zae-hyeong <p0138@naver.com>
2024-08-12 21:35:59 +0900

    [programmers] 행렬의 곱셈 solution 2, 3

--- Programmers/행렬의_곱셈.js
+++ Programmers/행렬의_곱셈.js
@@ -1,21 +1,32 @@
 function solution(arr1, arr2) {
-  
   const [rowLength1, colLength1] = [arr1.length, arr1[0].length];
   const [rowLength2, colLength2] = [arr2.length, arr2[0].length];
-  
+
   var answer = new Array(rowLength1);
   for (let i = 0; i < rowLength1; i++) {
     answer[i] = new Array(colLength2).fill(0);
   }
 
-  for (let i = 0; i < rowLength1; i++) 
-    for (let j = 0; j < colLength2; j++) 
-      for (let k = 0; k < colLength1; k++) 
-        answer[i][j] += arr1[i][k] * arr2[k][j]; 
-      
+  for (let i = 0; i < rowLength1; i++)
+    for (let j = 0; j < colLength2; j++)
+      for (let k = 0; k < colLength1; k++)
+        answer[i][j] += arr1[i][k] * arr2[k][j];
 
   return answer;
 }
+
+const solution2 = (arr1, arr2) =>
+  arr1.map((row) =>
+    arr2[0].map((x, y) => row.reduce((a, b, c) => a + b * arr2[c][y], 0))
+  );
+
+const solution3 = (arr1, arr2) => 
+  arr1.map(
+    (row) => row.map(
+      (_, i) => row.reduce(
+        (sum, cell, j) => sum + cell * arr2[j][i])
+      )
+  );
 
 const test = () => {
   console.log(
