26cf3bc75d5dd4293183a4b4ce8ddb4de8ac5b85
zae-hyeong <p0138@naver.com>
2024-08-26 21:27:04 +0900

    [programmers] 주식가격

--- Programmers/DataStructure/주식가격.js
+++ Programmers/DataStructure/주식가격.js
@@ -1,35 +1,30 @@
 function solution(prices) {
-  const stack = []; // {sec : 시간, stockPrice : 해당 시간의 주가 }
-  const answer = [];
+  const stack = [];
+  const answer = new Array(prices.length).fill(0);
 
-  for (let currentSec = 0; currentSec < prices.length; currentSec++) {
+  for (let current = 0; current < prices.length; current++) {
     if (
       stack.length === 0 ||
-      stack[stack.length - 1].stockPrice < prices[currentSec]
-    )
-      stack.push({
-        sec: currentSec,
-        stockPrice: prices[currentSec],
-      });
-    else {
-      while (stack[stack.length - 1].stockPrice > prices[currentSec]) {
-        const sec = stack.pop().sec;
-        answer[sec] = currentSec - sec;
-        stack.push({
-          sec: currentSec,
-          stockPrice: prices[currentSec],
-        });
-      }
+      prices[stack[stack.length - 1]] <= prices[current]
+    ) {
+      stack.push(current);
+      continue;
     }
+
+    while (prices[stack[stack.length - 1]] > prices[current]) {
+      const second = stack.pop();
+      answer[second] = current - second;
+    }
+    stack.push(current);
   }
 
-  const lastSec = prices.length - 1;
-  while (stack.length !== 0) {
-    const sec = stack.pop().sec;
-    answer[sec] = lastSec - sec;
+  while (stack.length) {
+    const second = stack.pop();
+    answer[second] = prices.length - second - 1;
   }
 
   return answer;
 }
 
-console.log(solution([1, 2, 3, 2, 3]));
+console.log(solution([1, 2, 3, 2, 3])); //4, 3, 1, 1, 0
+console.log(solution(	[1, 2, 3, 1, 2, 3, 3, 1, 2])); //	[8, 2, 1, 5, 3, 2, 1, 1, 0]
