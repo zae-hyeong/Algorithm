7d32fd32d261bf529fcba19e89a79e5c91fa6044
zae-hyeong <p0138@naver.com>
2025-01-15 22:33:43 +0900

    [Programmers] 프로세스

---
+++ Programmers/Level 2/프로세스.js
@@ -0,0 +1,76 @@
+class MaxHeap {
+  constructor(inputArr) {
+    this.arr = [...inputArr].sort((a, b) => a - b);
+  }
+
+  popMax() {
+    return this.arr.pop();
+  }
+
+  isEmpty() {
+    return this.arr.length === 0;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.arr = [];
+    this.head = 0;
+    this.tail = 0;
+  }
+
+  isEmpty() {
+    return this.head === this.tail;
+  }
+
+  push(data) {
+    this.arr.push(data);
+    this.tail++;
+  }
+
+  pop() {
+    const returnVal = this.arr[this.head++];
+    return returnVal;
+  }
+
+  size() {
+    return this.tail - this.head;
+  }
+}
+
+/**
+ *
+ * @param {Array} priorities
+ * @param {Number} location
+ */
+function solution(priorities, location) {
+  
+  const maxHeap = new MaxHeap(priorities);
+  const queue = new Queue();
+
+  const executionOrder = Array.from({length: priorities.length}, () => -1);
+
+  priorities.forEach((v, i) => {
+    queue.push(i);
+  });
+
+  let maxPriotiry = maxHeap.popMax();
+
+  while(!queue.isEmpty()) {
+    const priorityIndex = queue.pop();
+    const priority = priorities[priorityIndex];
+
+    if(priority === maxPriotiry) {
+      executionOrder[priorityIndex] = priorities.length - queue.size();
+
+      maxPriotiry = maxHeap.popMax();
+    } else {
+      queue.push(priorityIndex);
+    }
+  }
+
+  return executionOrder[location];
+}
+
+console.log(solution([2, 1, 3, 2], 2));
+console.log(solution([1, 1, 9, 1, 1, 1], 0));
