dedc904909b1ef5cd7df6f9744c536796e15a872
zae-hyeong <p0138@naver.com>
2025-02-24 22:00:59 +0900

    [BaekJoon] 1913 달팽이

---
+++ BaekJoon/Silver/1913_달팽이.js
@@ -0,0 +1,71 @@
+// let givenInput = [];
+// const readline = require("readline").createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+// readline
+//   .on("line", function (line) {
+//     givenInput.push(line.trim());
+//   })
+//   .on("close", function () {
+//     solution(givenInput);
+//     process.exit();
+//   });
+
+/**
+ * @param {Array<string>} inputLines
+ */
+function solution(inputLines) {
+  const [N, target] = inputLines.map((v) => +v);
+
+  const resultArr = Array.from({ length: N }, () =>
+    Array.from({ length: N }, () => null)
+  );
+
+  const moves = [
+    [1, 0],
+    [0, 1],
+    [-1, 0],
+    [0, -1],
+  ];
+  let moveIdx = 0;
+
+  const isValidMove = (y, x) =>
+    y >= 0 && y < N && x >= 0 && x < N && resultArr[y][x] === null;
+
+  let num = N ** 2;
+  let targetCor = [null, null];
+  let currentCor = [0, 0];
+
+  while (num > 0) {
+    if (num === target) targetCor = currentCor;
+    resultArr[currentCor[0]][currentCor[1]] = num--;
+
+    let [ny, nx] = [
+      currentCor[0] + moves[moveIdx][0],
+      currentCor[1] + moves[moveIdx][1],
+    ];
+    console.log("??", ny, nx);
+    if (isValidMove(ny, nx)) {
+      currentCor = [ny, nx];
+      continue;
+    }
+
+    moveIdx = moveIdx >= 3 ? 0 : moveIdx + 1;
+    [ny, nx] = [
+      currentCor[0] + moves[moveIdx][0],
+      currentCor[1] + moves[moveIdx][1],
+    ];
+    currentCor = [ny, nx];
+
+    console.table(resultArr);
+  }
+
+  const resultString = resultArr.map(v => v.join(' ')).join('\n');
+
+  console.log(resultString);
+  console.log(targetCor[0] + 1, targetCor[1] + 1);
+}
+
+solution(["7", "35"]);
+// solution(["5", "6"]);
