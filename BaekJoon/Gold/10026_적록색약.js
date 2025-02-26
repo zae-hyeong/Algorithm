23fee6022c97007a2c44244078a6d0419a1b0195
zae-hyeong <p0138@naver.com>
2025-02-26 13:34:46 +0900

    [BaekJoon, clean code] 10026 적록색약

--- BaekJoon/Gold/10026_적록색약.js
+++ BaekJoon/Gold/10026_적록색약.js
@@ -16,50 +16,51 @@ function solution(inputs) {
   const N = +inputs.shift();
   let picture = inputs.map((v) => v.split(""));
 
-  let numOfNormalArea = 0;
-  let numOfColorBlindArea = 0;
+  function checkNumOfArea() {
+    const visited = Array.from({ length: N }, () => Array(N).fill(false));
+    let num = 0;
 
-  const isValidMove = (y, x, color) =>
-    y >= 0 && y < N && x >= 0 && x < N && !visited[y][x] && picture[y][x] === color;
+    const moves = [
+      [0, 1],
+      [1, 0],
+      [-1, 0],
+      [0, -1],
+    ];
+  
+    const isValidMove = (y, x, color) =>
+      y >= 0 &&
+      y < N &&
+      x >= 0 &&
+      x < N &&
+      !visited[y][x] &&
+      picture[y][x] === color;
+  
+    const dfs = (y, x, color) => {
+      visited[y][x] = true;
+  
+      for (const [dy, dx] of moves) {
+        const [ny, nx] = [y + dy, x + dx];
+        if (isValidMove(ny, nx, color)) {
+          dfs(ny, nx, color);
+        }
+      }
+    };
 
-  const moves = [
-    [0, 1],
-    [1, 0],
-    [-1, 0],
-    [0, -1],
-  ];
-
-  const dfs = (y, x, color) => {
-    visited[y][x] = true;
-
-    for(const [dy, dx] of moves) {
-      const [ny, nx] = [y + dy, x + dx];
-      if (isValidMove(ny, nx, color)) {
-        dfs(ny, nx, color);
+    for (let i = 0; i < N; i++) {
+      for (let j = 0; j < N; j++) {
+        if (!visited[i][j]) {
+          dfs(i, j, picture[i][j]);
+          num++;
+        }
       }
     }
-  };
-
-  // normal
-  let visited = Array.from({ length: N }, () => Array(N).fill(false));
-
-  for (let i = 0; i < N; i++) {
-    for (let j = 0; j < N; j++) {
-      if (!visited[i][j]) {dfs(i, j, picture[i][j]);
-      numOfNormalArea++;}
-    }
+    return num;
   }
 
-  // colorBlind
-  visited = Array.from({ length: N }, () => Array(N).fill(false));
-  picture = picture.map(line => line.map((v) => (v === "R" || v === "G") ? "#" : v));
+  const numOfNormalArea = checkNumOfArea();
 
-  for (let i = 0; i < N; i++) {
-    for (let j = 0; j < N; j++) {
-      if (!visited[i][j]) {dfs(i, j, picture[i][j]);
-      numOfColorBlindArea++;}
-    }
-  }
+  picture = picture.map((line) => line.map((v) => (v === "R" || v === "G" ? "#" : v)));
+  const numOfColorBlindArea = checkNumOfArea();
 
   console.log(numOfNormalArea, numOfColorBlindArea);
 }
