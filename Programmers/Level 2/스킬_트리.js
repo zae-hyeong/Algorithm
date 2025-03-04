2b5f1dc24748705073ad785bf36ff0e5675f4f21
zae-hyeong <p0138@naver.com>
2025-03-04 22:15:51 +0900

    [Programmers, performance] 스킬트리(use Regular expression)

--- Programmers/Level 2/스킬_트리.js
+++ Programmers/Level 2/스킬_트리.js
@@ -27,4 +27,17 @@ function solution(skillOrder, skillTrees) {
   return cnt;
 }
 
+function solution(skill, skill_trees) {
+  var answer = 0;
+  var regex = new RegExp(`[^${skill}]`, 'g');
+
+  return skill_trees
+      .map((x) => x.replace(regex, ''))
+      .filter((x) => {
+          return skill.indexOf(x) === 0 || x === "";
+      })
+      .length
+}
+
+
 console.log(solution("CBD", ["BACDE", "CBADF", "AECB", "BDA"]));
