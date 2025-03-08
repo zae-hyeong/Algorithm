dcba80a6c556a3a93c3b32d5d444b25636bf663c
zae-hyeong <p0138@naver.com>
2025-03-08 22:52:13 +0900

    [BaekJoon] 16960 스위치와 램프

--- BaekJoon/Silver/16960_스위치와_램프.js
+++ BaekJoon/Silver/16960_스위치와_램프.js
@@ -18,22 +18,25 @@ readline
 function solution(inputLines) {
   const [N, M] = inputLines[0].split(" ").map((v) => +v);
   const [, ...inputs] = inputLines.map((v) => v.split(" ").map((v) => +v));
-  inputs.pop();
 
-  const isTurnOn = [true];
-  for (let i = 1; i <= M; i++) {
-    isTurnOn[i] = false;
-  }
-
-  let len = 0;
-  for(const input of inputs) {
-    len = input[0];
-    for (let i = 1; i <= len; i++) {
-      isTurnOn[input[i]] = true;
+  const nums = new Array(M + 1).fill(0);
+  for (const input of inputs) {
+    if (input[0] === 0) {
+      console.log(1);
+      return;
     }
+    for (let i = 1; i < input.length; i++) nums[input[i]]++;
   }
 
-  console.log(isTurnOn.reduce((a, v) => a && v) ? 1 : 0);
+  outer: for (const input of inputs) {
+    for (let i = 1; i < input.length; i++)
+      if (nums[input[i]] <= 1) continue outer; // 하나라도 대체 불가능한 숫자가 있다면
+
+    console.log(1);
+    return;
+  }
+
+  console.log(0);
 }
 
 solution(["4 5", "3 1 3 5", "1 2", "3 3 4 5", "1 1"]); // 1
