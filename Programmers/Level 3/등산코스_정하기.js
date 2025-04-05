5ddfaf9c674fff8aa7445b2336ff5c41d5c343e2
zae-hyeong <p0138@naver.com>
2025-04-05 15:59:38 +0900

    [Programmers, TimeOver] 등산코스 정하기

--- Programmers/Level 3/등산코스_정하기.js
+++ Programmers/Level 3/등산코스_정하기.js
@@ -12,49 +12,40 @@ const makeGraph = (paths) => {
     return map;
 };
 
-const makeCombination = (gates, summits) => {
-    const combination = [];
-    for (let i = 0; i < gates.length; i++) {
-        for (let j = 0; j < summits.length; j++) {
-            combination.push([gates[i], summits[j]]);
-        }
-    }
-
-    return combination;
-};
-
 function solution(n, paths, gates, summits) {
     const graph = makeGraph(paths);
 
     const answer = [];
+    const summitsSet = new Set(summits);
 
-    for (const [gate, summit] of makeCombination(gates, summits)) {
-        const result = [];
-
+    for (const gate of gates) {
         const queue = [];
+
         queue.push([gate, [], new Set([gate])]); // [point, pathWeights, visited]
 
         while (queue.length > 0) {
             const [point, pathWeights, visited] = queue.shift();
 
-            if (point === summit) {
-                result.push(Math.max(...pathWeights));
+            if (summitsSet.has(point)) {
+                answer.push([point, Math.max(...pathWeights)]);
                 continue;
             }
 
-            for(const {next: connectedPoint, weight} of graph.get(point)) {
-                
-                if(!visited.has(connectedPoint)) {
-                    queue.push([connectedPoint, [...pathWeights, weight], new Set([...visited.values(), connectedPoint])]);
+            for (const { next: connectedPoint, weight } of graph.get(point)) {
+                if (!visited.has(connectedPoint)) {
+                    queue.push([
+                        connectedPoint,
+                        [...pathWeights, weight],
+                        new Set([...visited.values(), connectedPoint]),
+                    ]);
                 }
             }
         }
-
-        answer.push([summit, Math.min(...result)]);
     }
 
-    return answer.sort(([summit1, intensity1], [summit2, intensity2]) => 
-        intensity1 - intensity2 || summit1 - summit2
+    return answer.sort(
+        ([summit1, intensity1], [summit2, intensity2]) =>
+            intensity1 - intensity2 || summit1 - summit2
     )[0];
 }
 
@@ -76,5 +67,20 @@ function solution(n, paths, gates, summits) {
 //     )
 // ); // [5, 3]
 // console.log(solution(7, [[1, 4, 4], [1, 6, 1], [1, 7, 3], [2, 5, 2], [3, 7, 4], [5, 6, 6]], [1], [2, 3, 4])); // [3,4]
-console.log(solution(7, 	[[1, 2, 5], [1, 4, 1], [2, 3, 1], [2, 6, 7], [4, 5, 1], [5, 6, 1], [6, 7, 1]], [3, 7], [1, 5])); //[5,1]
+console.log(
+    solution(
+        7,
+        [
+            [1, 2, 5],
+            [1, 4, 1],
+            [2, 3, 1],
+            [2, 6, 7],
+            [4, 5, 1],
+            [5, 6, 1],
+            [6, 7, 1],
+        ],
+        [3, 7],
+        [1, 5]
+    )
+); //[5,1]
 // console.log(solution(5, [[1, 3, 10], [1, 4, 20], [2, 3, 4], [2, 4, 6], [3, 5, 20], [4, 5, 6]], [1,2], [5])); // [5, 6]
