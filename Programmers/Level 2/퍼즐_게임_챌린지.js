14194bfee4ca55eb6af772bf04a99ab28f3473f1
zae-hyeong <p0138@naver.com>
2025-04-21 23:09:07 +0900

    [Programmers, TimeOver] 퍼즐 게임 챌린지

--- Programmers/Level 2/퍼즐_게임_챌린지.js
+++ Programmers/Level 2/퍼즐_게임_챌린지.js
@@ -1,26 +1,28 @@
 function solution(diffs, times, limit) {
-    let level = 1;
+    let level = -1;
+    for(const diff of diffs) {
+        if (level < diff) level = diff;
+    }
 
-    while (true) {
+    while (level >= 0) {
         let totalTime = 0;
 
         for (let [i, diff] of diffs.entries()) {
-            if (diff < level) totalTime += times[i];
-            else {
-                if (i === 0) {
-                    totalTime += times[i] * (diff - level);
-                    continue;
-                }
+            if (diff <= level) totalTime += times[i];
+            else if (i === 0) totalTime += times[i] * (diff - level);
+            else
                 totalTime +=
                     (times[i] + times[i - 1]) * (diff - level) + times[i];
-            }
         }
 
-        if (totalTime <= limit) return level;
-        level++;
+        if (totalTime > limit) return level + 1;
+        level--;
     }
+
+    return level + 1;
 }
 
+console.log(solution([0, 0, 0], [2, 4, 7], 30));
 console.log(solution([1, 5, 3], [2, 4, 7], 30));
 console.log(solution([1, 4, 4, 2], [6, 3, 8, 2], 59));
 console.log(solution([1, 328, 467, 209, 54], [2, 7, 1, 4, 3], 1723));
