70b5bf9ac9b54b5b465ed16380b3bf90fe2e114a
zae-hyeong <p0138@naver.com>
2025-04-29 15:19:57 +0900

    [Programmers] 2xN타일링

--- Programmers/Level 2/2xN타일링.js
+++ Programmers/Level 2/2xN타일링.js
@@ -1,34 +1,24 @@
-function makeFactorialArr(n) {
-    const arr = new Array(n + 1).fill(1);
-
-    for (let i = 2; i < n + 1; i++) {
-        arr[i] = arr[i - 1] * i;
-    }
-
-    return arr;
-}
-
 function solution(n) {
-    let answer = 0;
 
-    const fac = makeFactorialArr(n);
-    console.log(fac.at(5000))
+    let n1 = 1;
+    let n2 = 2;
+    if(n <= 2) return n;
 
-    let a = 0;
-    let b = n;
-    while (b >= 0) {
+    let num = 0 ;
 
-        answer += fac[a + b] / (fac[a] * fac[b]);
-        // console.log(a, b, fac[a + b] / (fac[a] * fac[b]))
-        a+=1;
-        b-=2;
+    for(let i = 3; i < n + 1; i ++) {
+        num = (n2 + n1) % 1_000_000_007;
+        [n1, n2] = [n2, num];
     }
 
-    return answer % 1_000_000_007;
+    return num;
 }
 
-// console.log(solution(1));
-// console.log(solution(4));
-// console.log(solution(6));
-// console.log(solution(7));
-console.log(solution(60000));
+console.log(solution(1));
+console.log(solution(2));
+console.log(solution(3));
+console.log(solution(4));
+console.log(solution(5));
+console.log(solution(6));
+console.log(solution(7));
+// console.log(solution(60000));
