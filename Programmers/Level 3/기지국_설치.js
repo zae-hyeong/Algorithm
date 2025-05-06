59e049ad2fcbb0441a1a31c86b70ac9a16336c4d
zae-hyeong <p0138@naver.com>
2025-05-06 13:04:45 +0900

    [Programmers, timeout, cleanCode]기지국_설치

--- Programmers/Level 3/기지국_설치.js
+++ Programmers/Level 3/기지국_설치.js
@@ -1,19 +1,20 @@
 function solution(n, stations, w) {
     let answer = 0;
-    
+
     let start = 1;
     const range = 2 * w + 1;
-    
-    stations.forEach((v, i) => {
-        const apts = v - w - start;
-        if(apts > 0) {
+
+    for (let i = 0; i < stations.length; i++) {
+        const apts = stations[i] - w - start;
+
+        if (apts > 0) {
             answer += Math.floor(apts / range);
-            if(apts % range > 0) answer += 1;
+            if (apts % range > 0) answer += 1;
         }
-        
-        start = v + w + 1;
-    });
-    
+
+        start = stations[i] + w + 1;
+    }
+
     if(n > stations.at(-1)) {
         const apts = n - start + 1;
         
@@ -25,3 +26,33 @@ function solution(n, stations, w) {
 
     return answer;
 }
+
+function timeout_solution(n, stations, w) {
+    let answer = 0;
+
+    let start = 1;
+    const range = 2 * w + 1;
+
+    for (let i = 0; i <= stations.length; i++) {
+        const apts = i !== stations.length ? stations[i] - w - start : n - start + 1; //timeout의 원인
+        if (apts > 0) {
+            answer += Math.floor(apts / range);
+            if (apts % range > 0) answer += 1;
+        }
+
+        start = stations[i] + w + 1;
+    }
+
+    return answer;
+}
+
+console.time();
+for(let i = 0; i < 100000; i++)
+    solution(200_000_000, Array.from({length: 10000}, (_, i) => i * 5), 1)
+console.timeEnd();
+
+
+console.time();
+for(let i = 0; i < 100000; i++)
+    timeout_solution(200_000_000, Array.from({length: 10000}, (_, i) => i * 5), 1)
+console.timeEnd();
