73d42cf9fafda67fb0699be3090df4571fb9b2bc
zae-hyeong <p0138@naver.com>
2025-05-07 23:02:11 +0900

    [중간저장] 1_구슬이_서말이라도

--- Original/1_구슬이_서말이라도.js
+++ Original/1_구슬이_서말이라도.js
@@ -14,33 +14,55 @@
 
 function solution(inputs) {
     const [, pearls, , ...temmpSlices] = inputs;
-    slices = temmpSlices.map((v) => v.split(" ").map((vv) => +vv));
+    const slices = temmpSlices.map((v) => v.split(" ").map((vv) => +vv));
 
-    
-    const answer = [];
-    
-    for (const [start, end] of slices) {
-        let slicedPearls = pearls.slice(start, end + 1);
-
-        let startIdx = slicedPearls.indexOf('*');
-        let lastIdx = slicedPearls.lastIndexOf('*');
-
-        if(startIdx === -1) {
-            answer.push(0);
-            continue;
-        }
-
-        const groups = slicedPearls.slice(startIdx+1, lastIdx).split('*');
-
-        let partialSum = 0;
-        for(const group of groups) {
-            partialSum += group.split('').map(v => +v).reduce((a, c) => a + c, 0);
-        }
-
-        answer.push(partialSum);
+    let startIdx = pearls.indexOf("*");
+    let lastIdx = pearls.lastIndexOf("*");
+    if (startIdx === -1) {
+        console.log(0);
+        return;
     }
 
-    console.log(answer.join("\n"));
+    const parts = pearls.split("*");
+    if (startIdx !== 0) parts.shift();
+    if (lastIdx !== pearls.length - 1) parts.pop();
+
+    console.log(parts);
+
+    const areas = []; // {start, end, price}
+    let currentIndex = startIdx;
+
+    for (const part of parts) {
+        const start = currentIndex;
+        const end = currentIndex + part.length;
+
+        let price = Array.from(part)
+            .map((v) => +v)
+            .reduce((a, c) => a + c, 0);
+        areas.push({ start, end, price });
+
+        currentIndex = end + 1;
+    }
+    console.log(areas);
+
+    const results = [];
+
+    for (const [start, end] of slices) {
+        let sum = 0;
+        let flag = false;
+        for (const area of areas) {
+            if (
+                (start >= area.start && start <= area.end) ||
+                (end >= area.start && end <= area.end)
+            ) {
+                flag = !flag;
+            }
+            if (flag) sum += area.price;
+        }
+        results.push(sum);
+    }
+
+    console.log(results.join("\n"));
 }
 
 solution(["20", "111*213*22*3*132**12", "4", "3 8", "10 18", "0 11", "4 9"]);
