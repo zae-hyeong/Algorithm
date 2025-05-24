b1a9b0bf21b84235944cf968c5f440080bd68d0b
zae-hyeong <p0138@naver.com>
2025-05-24 13:21:23 +0900

    [Python] 숫자 블록

--- Python/2Level/숫자_블록.py
+++ Python/2Level/숫자_블록.py
@@ -1,16 +1,21 @@
 def solution(begin, end):
     answer = []
-
-    for i in range(begin, end + 1) :
-        k = 0 if i == 1 else 1
-
-        for j in range(2, int(i**0.5) + 1):
-            if i % j == 0 and i // j <= 10000000:
-                k = i // j   # 제일 먼저 만나는 몫이 최댓값
-                break
-        
-        answer.append(k)
-
+    for n in range(begin, end + 1):
+        answer.append(maxBlock(n))
     return answer
 
-print(solution(1, 10))
+
+def maxBlock(n):
+    tmp = [1]
+    if n == 1:
+        return 0
+    for i in range(2, int(n ** (1 / 2)) + 1):
+        if n % i == 0 and i <= 10**7:
+            tmp.append(i)
+            if n // i <= 10**7 and n // i != n:
+                tmp.append(n // i)
+    return max(tmp)
+
+
+print(solution(["X591X", "X1X5X", "X231X", "1XXX1"]))
+# print(solution(["XXX","XXX","XXX"]))
