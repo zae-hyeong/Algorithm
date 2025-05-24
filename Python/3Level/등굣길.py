09431d0d2047ad1c8f4e7a00f90aa0b857f66fc2
zae-hyeong <p0138@naver.com>
2025-05-24 14:27:59 +0900

    [Python, Fail] 등굣길

--- Python/3Level/등굣길.py
+++ Python/3Level/등굣길.py
@@ -4,28 +4,40 @@ from collections import deque
 def solution(m, n, puddles):
     answer = 0
 
-    puddleHash = set(list(map(lambda puddle: f"{puddle[1] - 1},{puddle[0] - 1}", puddles)))
+    puddleHash = set(
+        list(map(lambda puddle: f"{puddle[1] - 1},{puddle[0] - 1}", puddles))
+    )
+
+    visited = [([0] * m) for _ in range(n)]
 
     def _isValidMove(y, x):
-        return y < n and x < m and f"{y},{x}" not in puddleHash
+        return (
+            y >= 0
+            and y < n
+            and x >= 0
+            and x < m
+            and f"{y},{x}" not in puddleHash
+            and visited[y][x] == 0
+        )
 
     queue = deque()
-    queue.append([0, 0])
+    queue.append([n - 1, m - 1])
+    visited[n - 1][m - 1] = 1
 
     while len(queue) > 0:
         [y, x] = queue.popleft()
-        print(y, x)
 
-        if y == n - 1 and x == m - 1 :
-            answer += 1
-            continue
-        
-        for dy, dx in [[0, 1], [1, 0]]:
+        for dy, dx in [[-1, 0], [0, -1]]:
             ny, nx = y + dy, x + dx
+
             if _isValidMove(ny, nx):
                 queue.append([ny, nx])
+                if ny < n - 1:
+                    visited[ny][nx] += visited[ny + 1][nx] % 1_000_000_007
+                if nx < m - 1:
+                    visited[ny][nx] += visited[ny][nx + 1] % 1_000_000_007
 
-    return answer % 1_000_000_007
+    return visited[0][0]
 
 
 print(solution(4, 3, [[2, 2], [2, 3]]))
