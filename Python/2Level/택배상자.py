f5ff3abd1c1c2dae443e6a4dac540038a0f9ed25
zae-hyeong <p0138@naver.com>
2025-05-24 12:27:44 +0900

    [Python, CleanCode] 택배상자

--- Python/2Level/택배상자.py
+++ Python/2Level/택배상자.py
@@ -1,65 +1,21 @@
-from collections import deque
-
-
 def solution(order):
 
-    items = deque()
-    for i in range(1, len(order) + 1):
-        items.append(i)
+    stack = []
+    idx = 0
+    N = len(order)
 
-    stack = deque()
+    for i in range(1, N + 1):
+        stack.append(i)
 
-    topItems = set()
-    topItems.add(1)
+        while stack[-1] == order[idx]:
+            idx += 1
+            stack.pop()
+            if len(stack) == 0:
+                break
 
-    def pushToStack(v):
-        if len(stack) > 0:
-            lastItem = stack[len(stack) - 1]
-            topItems.remove(lastItem)
+        i += 1
 
-        stack.append(v)
-        topItems.add(v)
-
-    def popFromStack():
-        item = stack.pop()
-        topItems.remove(item)
-        if len(stack) > 0:
-            topItems.add(stack[len(stack) - 1])
-        return item
-
-    def popFromItems():
-        item = items.popleft()
-        topItems.remove(item)
-
-        if len(items) > 0:
-            topItems.add(items[0])
-
-        return item
-
-    count = 0
-    for target in order:
-        if target not in topItems:
-            if target < items[0]:
-                return count
-
-            while target > items[0]:
-                v = popFromItems()
-                pushToStack(v)
-
-            popFromItems()
-            count += 1
-
-        else:
-            if len(stack) > 0 and target == stack[len(stack) - 1]:
-                popFromStack()
-                count += 1
-            elif target == items[0]:
-                popFromItems()
-                count += 1
-
-        # print(items, stack, topItems)
-
-    return count
+    return idx
 
 
 print(solution([4, 3, 1, 2, 5]))
